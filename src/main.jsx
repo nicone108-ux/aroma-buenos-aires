8e008dd76a3017c08b41862c03ead69417c1045a
Nicolas Yapura Olivera <nicolino@MacBook-Pro-de-Nicolas.local>
2026-05-16 18:53:42 -0300

    correccion final para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
-//import React from "react";
-//import ReactDOM from "react-dom/client";
 import { createRoot } from 'react-dom/client'
 import App from "./App.jsx";
-//import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext.jsx";
 
